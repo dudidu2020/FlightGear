ab208a217f1a61a0e16f08553eaa95a5a3add0a9
David Galustov <david.galustov@live.biu.ac.il>
2021-06-23 16:57:42 +0300

    check

--- app/src/main/java/com/example/flightgear10/MainActivity.java
+++ app/src/main/java/com/example/flightgear10/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.flightgear10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     @Override
